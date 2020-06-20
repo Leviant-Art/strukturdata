9341e3cde471c7410644e1f4440d8ec7cce34a9d
TeukuIkhwanulAkbar <67189544+TeukuIkhwanulAkbar@users.noreply.github.com>
2020-06-20 18:57:14 +0700

    praktikum struktur data

---
+++ DemoStackPopString.java
@@ -0,0 +1,28 @@
+package demostackpopstring;
+
+import java.util.*;
+
+public class DemoStackPopString {
+
+    public static void main(String[] args) {
+        // Creating an empty Stack
+        Stack<String> STACK = new Stack<String>();
+        
+        STACK.push("Minggu");
+        STACK.push("Senin");
+        STACK.push("Selasa");
+        STACK.push("Rabu");
+        STACK.push("kamis");
+        
+        // Displaying the stack
+        System.out.println("Stack Awal: " + STACK);
+        
+        //Removing elements using pop() method
+        System.out.println("Pop Element: " + STACK.pop());
+        System.out.println("Pop Element: " + STACK.pop());
+        
+        // Display the Stack after pop operation
+        System.out.println("Update Stack: " + STACK);
+    }
+    
+}
